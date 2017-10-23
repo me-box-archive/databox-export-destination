75c783d66749644cfec2ece448c52ee54cb56afb
Yousef Amar <yousefamar@gmail.com>
2017-10-23 11:16:40 +0200

    Implement namespacing

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const url = require('url');
 const app = require('express')();
 const bodyParser = require('body-parser');
 const server = require('http').createServer(app);
@@ -7,11 +8,20 @@ app.disable('x-powered-by');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
+io.on('connection', (socket) => {
+	socket.join(url.parse(socket.request.headers.referer).pathname);
+});
 app.use((req, res, next) => {
-	io.sockets.emit('request', Date.now(), req.method, req.path, req.query, req.body);
+	io.to('/').emit('request', Date.now(), req.method, req.path, req.query, req.body);
+	let dirs = req.path.substr(1).split('/');
+	let namespace = '';
+	for (let dir of dirs) {
+		namespace += '/' + dir;
+		io.to(namespace).emit('request', Date.now(), req.method, req.path.substr(namespace.length), req.query, req.body);
+	}
 	next();
 });
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
 	res.render('index');
 	res.send();
 });
